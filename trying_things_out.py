58386dc4ba8e93a25ad84be331819527f7944f0e
andresturu <andresturu@icloud.com>
2025-06-30 20:54:35 -0700

    j

---
+++ trying_things_out.py
@@ -0,0 +1,6 @@
+def main(): #def main() is optional, except in some cases, so just be careful in the future
+    print("hello")
+
+
+if __name__ == '__main__': # this is optional except in some cases, def main and this thing go hand in hand
+    main()
